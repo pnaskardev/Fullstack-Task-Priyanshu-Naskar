6368632fec32c0163deed6d9fb2728117a9c3920
pnaskardev <pnaskardev@gmail.com>
2024-10-11 14:51:26 +0530

    Refactor socket URL to use remote IP address

--- web/src/api/socket.ts
+++ web/src/api/socket.ts
@@ -4,6 +4,6 @@ import socketIOClient from "socket.io-client";
 // but this should be failrly easy task
 
 const PORT = 3000;
-const socket = socketIOClient(`http://localhost:${PORT}`);
+const socket = socketIOClient(`http://65.0.199.40:${PORT}`);
 // const socket = socketIOClient(`${baseURL}`);
 export default socket;
