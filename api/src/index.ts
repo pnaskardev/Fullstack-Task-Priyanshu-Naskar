4d078ac392d944a5d92a17d5d3a2706e415aca1f
pnaskardev <pnaskardev@gmail.com>
2024-10-11 15:38:04 +0530

    Refactor app.ts to bind server to 0.0.0.0

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,15 +9,18 @@ import MongoService from "./service/dbService";
 const main = async () => {
   const config = await getConfig();
   const port = config.port || 3100;
+  const host = '0.0.0.0'; // Binding to 0.0.0.0
   const socketService = new SocketService();
+  
   await MongoService.connect(config.database);
   await RedisService.connect(config.cache);
 
   const app = await createApp();
   const server = createServer(app);
   socketService.io.attach(server);
-  server.listen(port, () => {
-    logger.info(`Started listening on port ${port}`);
+
+  server.listen(port, host, () => { // Binding to 0.0.0.0 here
+    logger.info(`Started listening on port ${port} at host ${host}`);
   });
 
   socketService.initListeners();
